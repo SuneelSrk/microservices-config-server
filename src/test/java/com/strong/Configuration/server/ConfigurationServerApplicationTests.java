88ce96c579a3ff0885271f19fd535027789b77b6
SuneelSrk <59217856+SuneelSrk@users.noreply.github.com>
2021-06-03 17:30:43 +0530

    initial commit

---
+++ src/test/java/com/strong/Configuration/server/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strong.Configuration.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
